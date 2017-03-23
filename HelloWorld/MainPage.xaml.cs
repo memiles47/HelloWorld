e8ef5a434ec05642a7fad8effdca260b7e927f9a
Michael E Miles <michael@miles-me.net>
2017-03-23 03:15:28 -0500

    Add Hello World to Application. Commit and check sync with other computer

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-//Testing this commit
+//Love you Angie
 
 namespace HelloWorld
 {
