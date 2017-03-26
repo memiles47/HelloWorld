7b4f00ca1276204a293195cf512a838223df5bb8
Michael E Miles <michael@miles-me.net>
2017-03-26 09:54:36 -0500

    Testing

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 //Night Babe - Secret
+//What a wonderful person.
 
 namespace HelloWorld
 {
