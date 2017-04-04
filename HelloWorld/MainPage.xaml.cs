96dcdaa58b0ff8a03bada59c2d1f4e3d89b13663
Michael E Miles <michael@miles-me.net>
2017-04-04 03:15:17 -0500

    Now time to figure out why things are not working

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 //Night Babe - Secret
-//Miss her
+//Miss her - Great phone call last night
 namespace HelloWorld
 {
     /// <summary>
